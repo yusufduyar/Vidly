3b46547c51dfb6e8bea26f6f0529c41559d47b3a
Yusuf Duyar <yusufduyar@outlook.com>
2017-09-11 00:22:13 +0300

    Added new properties to movies

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,53 +4,32 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using System.Data.Entity;
 using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
-        public ActionResult Random()
+        public ApplicationDbContext _context;
+
+        public MoviesController()
         {
-            var movie = new Movie()
-            {
-                Name = "Shrek!"
-            };
-            var customers = new List<Customer>
-            {
-                new Customer() {Name = "Customer 1"},
-                new Customer() {Name = "Customer 2"}
-            };
-
-            var randomMovieViewModel = new RandomMovieViewModel()
-            {
-                Movie = movie,
-                Customers = customers
-            };
-
-            return View(randomMovieViewModel);
-        }
-
-        public ActionResult Edit(int id)
-        {
-            return Content($"id={id}");
+            _context = new ApplicationDbContext();
         }
 
         public ActionResult Index()
         {
-            var movies = new List<Movie>
-            {
-                new Movie() {Id=1,Name="Shrek" },
-                new Movie() {Id=2, Name="Wall-e" }
-            };
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
+
             return View(movies);
         }
         
-        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
+        public ActionResult Details(int id)
         {
-            return Content(year + "/" + month);
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+
+            return View(movie);
         }
     }
 }
