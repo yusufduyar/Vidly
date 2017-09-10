c2bd44fd9c9bf311536b0ba26d5e82d7b318593c
Yusuf Duyar <yusufduyar@outlook.com>
2017-09-10 21:53:38 +0300

    Changed customer data source to database.

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,24 +11,27 @@ namespace Vidly.Controllers
     [RoutePrefix("customers")]
     public class CustomersController : Controller
     {
-        private IEnumerable<Customer> GetCustomers()
+        private ApplicationDbContext _context;
+
+        public CustomersController()
         {
-            return new List<Customer>
-            {
-                new Customer { Id = 1, Name = "Yusuf DUYAR" },
-                new Customer { Id = 2, Name = "Melike DUYAR" }
-            };
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
         }
         
         public ActionResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.Include(c=> c.MembershipType).ToList();
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var selectedCustomer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var selectedCustomer = _context.Customers.SingleOrDefault(c => c.Id == id);
             if (selectedCustomer == null) return HttpNotFound();
             return View(selectedCustomer);
         }
