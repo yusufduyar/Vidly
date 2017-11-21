30cf938dd39eb29e418d7837a31739074f8fb53f
Yusuf Duyar <yusufduyar@outlook.com>
2017-11-21 13:00:25 +0300

    Validations added to customer class

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -21,6 +21,7 @@ namespace Vidly.Models
         [Display(Name="Membership Type")]
         public byte MembershipTypeId { get; set; }
 
+        [Min18YearsIfAMember]
         public DateTime? BirthDate { get; set; }
     }
 }
